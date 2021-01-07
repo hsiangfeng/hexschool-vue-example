0330b5d13bc0609becc5b2eaeb7e703ccc39be96
Ray <loveyou520babytw@gmail.com>
2021-01-07 11:14:13 +0800

    updata

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -81,13 +81,14 @@ export default {
   name: 'Home',
   data() {
     return {
-      isLoading: false,
-      data: [],
-      carts: [],
-      carData: JSON.parse(localStorage.getItem('carData')) || [],
+      isLoading: false, // 讀取狀態
+      data: [], // 產品列表資料放置處
+      carts: [], // 購物車內容
+      carData: JSON.parse(localStorage.getItem('carData')) || [], // localStorage 資料
     };
   },
   methods: {
+    // 取得產品列表
     getProducts() {
       this.isLoading = true;
       this.axios.get(`${process.env.VUE_APP_APIPATH}/api/${process.env.VUE_APP_COUSTOMPATH}/products/all`)
@@ -96,6 +97,7 @@ export default {
           this.isLoading = false;
         });
     },
+    // 取得購物車內容
     getCarts() {
       this.isLoading = true;
       this.axios.get(`${process.env.VUE_APP_APIPATH}/api/${process.env.VUE_APP_COUSTOMPATH}/cart`)
@@ -104,12 +106,12 @@ export default {
           this.isLoading = false;
         });
     },
+    // 加入購物車(localStorage)
     addtoCarts(data) {
       const cacheCarID = []; // 暫存 ID 放置處
 
       // 一開始先將 carData 中的 ID 全部撈出來
-      this.carData.forEach((item, key) => {
-        console.log('key', key);
+      this.carData.forEach((item) => {
         cacheCarID.push(item.product_id);
       });
 
@@ -118,70 +120,86 @@ export default {
       // 若存在則會回傳 0 並改走 else
       if (cacheCarID.indexOf(data.id) === -1) {
         const cartContent = {
-          product_id: data.id,
-          qty: 1,
-          name: data.title,
+          product_id: data.id, // 產品 ID
+          qty: 1, // 產品數量，預設一筆
+          name: data.title, // 產品標題
         };
+        // 將數量推回陣列中
         this.carData.push(cartContent);
+        // 重新寫入 localStorage
         localStorage.setItem('carData', JSON.stringify(this.carData));
       } else {
-        let cache = {};
+        let cache = {}; // 產品暫存處
         this.carData.forEach((item, keys) => {
+          // 只找相同的產品內容
           if (item.product_id === data.id) {
-            let { qty } = item;
+            let { qty } = item; // 取出已存在 localStorage 購物車的資料並加數量增加
             cache = {
-              product_id: data.id,
-              qty: qty += 1,
-              name: data.title,
+              product_id: data.id, // 產品 ID
+              qty: qty += 1, // 產品當前數量，針對數量增加數量
+              name: data.title, // 產品標題
             };
+            // 移除現有 localStorage 購物車的資料，否則 localStorage 會重複加入
             this.carData.splice(keys, 1);
           }
         });
-        this.carData.push(cache);
+        // 將數量已經增加的資料推回陣列中
+        this.carData.push(cache); // 不建議放在 forEach 內，否則迴圈會重複執行導致變成加二
+        // 重新寫入 localStorage
         localStorage.setItem('carData', JSON.stringify(this.carData));
       }
     },
+    // 送出購物車資料
     postCarts() {
       this.isLoading = true;
       // 避免重複加入因此要先跑一次清空購物車
       const cacheID = [];
       this.axios.get(`${process.env.VUE_APP_APIPATH}/api/${process.env.VUE_APP_COUSTOMPATH}/cart`)
         .then((res) => {
+          // 取得「當前購物車內」的產品
           const cacheData = res.data.data.carts;
+          // 撈出產品 ID 一一加入到暫存的陣列
           cacheData.forEach((item) => {
             cacheID.push(item.id);
           });
         }).then(() => {
-          // 清除購物車
+          // 清空「當前購物車內」的資料，否則會重複加入
           cacheID.forEach((item) => {
             this.axios.delete(`${process.env.VUE_APP_APIPATH}/api/${process.env.VUE_APP_COUSTOMPATH}/cart/${item}`).then(() => {
               console.log('購物車已經清空');
             });
           });
         }).then(() => {
+          // 將當前暫存在 carData 的資料撈出來
           this.carData.forEach((item) => {
+            // 取出產品 id 與數量
             const cache = {
               product_id: item.product_id,
               qty: item.qty,
             };
+            // 一一加入到購物車內
             this.axios.post(`${process.env.VUE_APP_APIPATH}/api/${process.env.VUE_APP_COUSTOMPATH}/cart`, { data: cache })
               .then(() => {
+                // 確保送出訂單之後清空購物車內容
                 this.carData = [];
                 localStorage.removeItem('carData');
                 this.isLoading = false;
+                // 重新取得購物車內容
                 this.getCarts();
               });
           });
         });
     },
+    // 清空購物車
     deleteCarts() {
       this.isLoading = true;
       const cacheID = [];
+      // 取得「當前購物車內」的產品
       this.axios.get(`${process.env.VUE_APP_APIPATH}/api/${process.env.VUE_APP_COUSTOMPATH}/cart`)
         .then((res) => {
           const cacheData = res.data.data.carts;
           cacheData.forEach((item) => {
-            cacheID.push(item.id);
+            cacheID.push(item.id); // 取出購物車內的 ID
           });
         }).then(() => {
           // 清除購物車
@@ -191,13 +209,17 @@ export default {
               this.getCarts();
             });
           });
+        }).then(() => {
+          this.isLoading = false;
         });
     },
+    // 清空畫面上的資料並重新設置 localStorage
     deleteLocalStorage() {
       localStorage.removeItem('carData');
       this.carData = JSON.parse(localStorage.getItem('carData')) || [];
     },
   },
+  // 取得產品列表與購物車內容
   created() {
     this.getProducts();
     this.getCarts();
